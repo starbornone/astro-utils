ce1353711770f3b8daafc6bbd3c5c0105db27b5c
Sha <sha.kongbrooks@gmail.com>
2024-12-21 05:39:53 +1100

    Add some basic constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+// Offset for Julian Date calculations
+export const JULIAN_DATE_0 = 0.0009;
+
+// Julian Date for Unix epoch (January 1, 1970)
+export const JULIAN_DATE_1970 = 2440588;
+
+// Julian Date for J2000 epoch (January 1, 2000)
+export const JULIAN_DATE_2000 = 2451545;
+
+// Mathematical constant π
+export const PI = Math.PI;
+
+// Number of milliseconds in one day
+export const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// Conversion factor from degrees to radians
+export const DEGREES_TO_RADIANS = PI / 180;
